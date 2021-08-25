cf8cb8c93e52c9b5b8cbcdbcc0f6306d5e9ba365
arnas <arnas.bosas@adeoweb.biz>
2021-08-25 08:18:57 +0300

    (WA-3) Created a user-preferred weather type for a user table.

---
+++ Controller/Index/Index.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AdeoWeb\WeatherApplication\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\Controller\ResultInterface;
+use Magento\Framework\View\Result\Page;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action
+{
+    /**
+     * @var PageFactory
+     */
+    private $resultPageFactory;
+
+    public function __construct(
+        Context $context,
+        PageFactory $pageFactory
+    ) {
+        $this->resultPageFactory = $pageFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * @return Page|ResultInterface
+     */
+    public function execute()
+    {
+        return $this->resultPageFactory->create();
+    }
+}
--- Model/ResourceModel/WeatherType.php
+++ Model/ResourceModel/WeatherType.php
@@ -8,10 +8,11 @@ use Magento\Framework\Model\ResourceModel\Db\AbstractDb;
 
 class WeatherType extends AbstractDb
 {
-    private const TABLE_NAME = 'adeoweb_weatherapplication_weathertype';
+    private const TABLE_NAME    = 'adeoweb_weatherapplication_weathertype';
+    private const FIELD_NAME_ID = 'weathertype_id';
 
     protected function _construct()
     {
-        $this->_init(self::TABLE_NAME, 'weathertype_id');
+        $this->_init(self::TABLE_NAME, self::FIELD_NAME_ID);
     }
 }
